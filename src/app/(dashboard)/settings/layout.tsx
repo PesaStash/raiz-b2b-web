f553ac764c887d45e37479f03c8b67237bb5801e
raiz-olawunmidada <olawunmi.dada@raiz.app>
2026-02-24 23:26:11 +0100

    feat: Implement new UI styling for settings pages and transaction table, including layout, backgrounds, borders, and rounded corners.

--- src/app/(dashboard)/settings/layout.tsx
+++ src/app/(dashboard)/settings/layout.tsx
@@ -3,9 +3,11 @@ import SideLayout from "./_components/SideLayout";
 
 const layout = ({ children }: { children: ReactNode }) => {
   return (
-    <section className="border-t border-[#dedce2] w-full flex gap-[25px] px-8 -ml-8 h-full relative ">
+    <section className=" w-full flex gap-[25px] px-8 -ml-8 h-screen  relative bg-[#F8F7FA] ">
       <SideLayout />
-      <section className="w-[77%] ml-6 xl:ml-8 pt-8 ">{children}</section>
+      <section className="w-[77%] ml-6 xl:ml-8 pt-8 bg-raiz-gray-50 p-8 pb-4  rounded-[20px] border-[1.5px] border-raiz-gray-200">
+        {children}
+      </section>
     </section>
   );
 };
