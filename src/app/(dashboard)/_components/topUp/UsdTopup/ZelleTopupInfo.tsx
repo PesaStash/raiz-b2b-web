ed4928b678c75fe613f1262e450b0aa5a83a2649
raiz-olawunmidada <olawunmi.dada@raiz.app>
2026-03-23 09:17:56 +0100

    feat(zelle-topup):RAZ-1162 Add Zelle third-party payment warnings and verification requirements to top-up instructions.

--- src/app/(dashboard)/_components/topUp/UsdTopup/ZelleTopupInfo.tsx
+++ src/app/(dashboard)/_components/topUp/UsdTopup/ZelleTopupInfo.tsx
@@ -23,9 +23,13 @@ const ZelleTopupInfo = ({ goBack, goNext, type }: Props) => {
             {type === "guest" ? "Send" : "Top up"} via Zelle
           </h3>
           <p className="text-yellow-500 font-medium">Important:</p>
-          <p className="text-zinc-900 text-xs leading-tight">
+          <p className="text-zinc-900 text-left text-xs leading-tight">
             Make sure the memo code and amount matches or funds won&apos;t be
             credited.
+          </p>
+          <p className="text-yellow-500 text-left text-xs leading-tight">
+            Zelle payments must come from an account in your name. Third party
+            payments will be require verifications before being credited.
           </p>
         </div>
         <div className="flex flex-col gap-5 mb-5">
@@ -48,12 +52,22 @@ const ZelleTopupInfo = ({ goBack, goNext, type }: Props) => {
             copyable
           />
           <ListDetailItem
-            border
             title="Expires at"
             value={dayjs(zelleInfo?.expires_at || "").format(
-              "DD/MM/YYYY, h:mm:ss A"
+              "DD/MM/YYYY, h:mm:ss A",
             )}
           />
+          <div className="rounded-md text-xs p-2 bg-zinc-100">
+            <p className="font-semibold mb-1 ">
+              If verification is required, you'll need to provide:
+            </p>
+            <div className="flex flex-col gap-1">
+              <li className="list-disc ml-4">The name on Zelle account</li>
+              <li className="list-disc ml-4">
+                The phone number or email connected to the zelle account.
+              </li>
+            </div>
+          </div>
         </div>
         <Button onClick={goNext}>I&apos;ve completed the Zelle transfer</Button>
       </div>
