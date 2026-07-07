98c18566380ab8497aa2e66891ee139d8d705a2e
raiz-olawunmidada <olawunmi.dada@raiz.app>
2026-07-07 14:28:54 +0100

    feat(analytics): integrate Google Tag Manager for enhanced tracking
    - Added Google Tag Manager script to the layout for improved analytics tracking.
    - Included noscript fallback for users with JavaScript disabled.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -113,6 +113,19 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
+        {/* Google Tag Manager */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','GTM-TSL75KND');
+            `,
+          }}
+        />
+        {/* End Google Tag Manager */}
         <link
           rel="apple-touch-icon"
           sizes="180x180"
@@ -131,10 +144,21 @@ export default function RootLayout({
           href="/favicon/favicon-16x16.png"
         />
         <link rel="manifest" href="/favicon/site.webmanifest"></link>
+        
       </head>
       <body
         className={`${inter.variable}  ${monzoSans.variable} ${brSonoma.variable} `}
       >
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-TSL75KND"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          />
+        </noscript>
+        {/* End Google Tag Manager (noscript) */}
         <Provider>
           <MainLayout>
             <Suspense fallback={<Loading />}>{children}</Suspense>
